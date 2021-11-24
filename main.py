7f3a3e0aac079d0f288557e04c007983dbefff9b
Saintmantis1021 <arunbabu1021@gmail.com>
2021-11-24 11:19:48 +0530

    1

---
+++ main.py
@@ -0,0 +1,63 @@
+#god is great
+#and
+import eel
+from  tkinter import *
+from  tkinter import filedialog
+from tkinter.filedialog import asksaveasfile
+import img2pdf
+from PIL import Image
+import os
+
+
+eel.init('web')
+
+imgtopdf_img_array=[]
+imgtopdf_pdf_array=[]
+
+@eel.expose
+def select_img():
+    img_path = filedialog.askopenfilename()
+    print(img_path)
+    imgtopdf_img_array.append(img_path)
+
+@eel.expose
+def save_file():
+    files = [('All Files', '*.*'), ('Python Files', '*.py'),('Text Document', '*.txt')]
+    file = asksaveasfile(filetypes = files, defaultextension = files)
+    pdf_path=file.name
+    imgtopdf_pdf_array.append(pdf_path)
+    print(pdf_path)
+
+
+@eel.expose
+def convert_img_to_pdf():
+    def ImgtoPdf(img_path,pdf_path):
+        print("started")
+        image = Image.open(img_path)
+        pdf_bytes = img2pdf.convert(image.filename)
+        file = open(pdf_path, "wb")
+        file.write(pdf_bytes)
+        image.close()
+        file.close()
+        print("Successfully made pdf file")
+
+    ImgtoPdf(imgtopdf_img_array[0],imgtopdf_pdf_array[0])
+
+
+
+
+
+
+
+
+
+eel.start('imgtopdf.html')
+
+
+
+
+
+
+
+
+    
